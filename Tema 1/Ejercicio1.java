44b055bb3d36fd27c862d8abf19597cfad099593
David Balaguer <davidbalaguercervera@gmail.com>
2021-09-27 09:07:54 +0200

    Acabadeos los dos primeros ejemplos del tema 1

---
+++ Tema 1/Ejercicio1.java
@@ -0,0 +1,17 @@
+
+class Ejercicio1
+{
+    
+    public static void main(String args[])
+    {
+	double dividendo = 20.0;
+	
+	double divisor = 6.0;
+
+	double suma = 3.0;
+
+	double resultado = (dividendo/divisor)+suma;
+
+        System.out.print(resultado);
+    }
+}
