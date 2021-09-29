82c8ea0fed0967e16df682235b9016722b5a4941
Davcer99 <davidbalaguercervera@gmail.com>
2021-09-29 22:34:39 +0200

    Ejercicio 4 terminado

--- Tema 1/Ejercico4.java
+++ Tema 1/Ejercico4.java
@@ -1,10 +1,14 @@
+import java.util.*;
 class Ejercicio4
 {
     
     public static void main(String args[])
     {
-	    int lado = 5;
-	
+	    Scanner entrada=new Scanner(System.in);
+        
+        System.out.println("Enter the side: ");
+        
+        int lado= entrada.nextInt();
 	    int resultado = lado*lado;
 
 	    System.out.print(resultado);
