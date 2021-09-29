3b00d0d318090c8d465603e439b571bc4498bc53
Davcer99 <davidbalaguercervera@gmail.com>
2021-09-29 22:38:02 +0200

    sintaxis

--- Tema 1/Ejercico4.java
+++ Tema 1/Ejercico4.java
@@ -4,12 +4,13 @@ class Ejercicio4
     
     public static void main(String args[])
     {
-	    Scanner entrada=new Scanner(System.in);
+	    Scanner entrada = new Scanner(System.in);
         
         System.out.println("Enter the side: ");
         
         int lado= entrada.nextInt();
-	    int resultado = lado*lado;
+	    
+        int resultado = lado*lado;
 
 	    System.out.print(resultado);
     }
