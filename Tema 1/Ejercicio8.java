3c76f649a7ed18da185fd800b0cd2bfdf5d8c5b9
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-03 13:43:59 +0200

    Ejercicio 8

---
+++ Tema 1/Ejercicio8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String args[]) {
+        double edad;
+          
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        
+        System.out.println("Introduce tu edad ");
+
+        edad = entradaTeclado.nextFloat();
+
+        entradaTeclado.close();
+
+        if (edad >= 18){
+            System.out.println("Mayor de edad");
+        }else{
+            System.out.println("Menor de edad");
+        }
+    }
+}
