0efac0951fbb906e8431becb33048546b456d918
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-03 14:55:10 +0200

    Ejercicio 11 y12

--- Tema 1/Ejercicio12.java
+++ Tema 1/Ejercicio12.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-class Ejercicio11 {
+class Ejercicio12 {
   
     public static void main(String args[]) {
      
         float valor1;
         float valor2;
-        float mayor;
-        float menor;
+        float mayor_valor;
+        
         
        
         Scanner entradaTeclado;
@@ -24,14 +24,12 @@ class Ejercicio11 {
         entradaTeclado.close();
 
         if (valor1 > valor2){
-             mayor = valor1;
-             menor = valor2;
+             mayor_valor = valor1;
         }else{
-            mayor = valor2;
-            menor = valor1;
+            mayor_valor = valor2;
         }
 
-	    System.out.println("El orden de los numeros en orden de valor ascendente es el siguiente: " + menor + "  " + mayor );  
+	    System.out.println("El valor mayor es: "+ mayor_valor);  
     }
     
 }
