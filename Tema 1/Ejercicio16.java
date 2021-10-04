9cd63759ef8b08e193d663882b6fefa6eedf5441
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-04 22:12:47 +0200

    Ejercicio 16

--- Tema 1/Ejercicio16.java
+++ Tema 1/Ejercicio16.java
@@ -4,24 +4,35 @@ class Ejercicio16 {
   
     public static void main(String args[]) {
      
-        double valor1, valor2;
-        char operacion;
+        int valor1, valor2;
+        String operacion;
+        int resultado = 0;
         
         Scanner entradaTeclado;
         entradaTeclado = new Scanner(System.in);
 
         System.out.println("Introduce el valor  ");
-        valor1 = entradaTeclado.nextFloat();
+        valor1 = entradaTeclado.nextInt();
 
         System.out.println("Introduce la operacion que desea realizar + - *  /");
         operacion = entradaTeclado.next();
         
         System.out.println("Introduce el valor ");
-        valor2 = entradaTeclado.nextFloat(); 
+        valor2 = entradaTeclado.nextInt(); 
 
         entradaTeclado.close();
 
-	    System.out.println("El valor mayor es: ");  
+        if (operacion.equals("+")){
+            resultado = valor1 + valor2;
+        }else if (operacion.equals("-")) {
+            resultado = valor1 - valor2;
+        }else if (operacion.equals("*")) {
+            resultado = valor1 * valor2;
+        }else if (operacion.equals("/")) {
+            resultado = valor1 / valor2;
+        }
+
+	    System.out.println("El resultado de la operacion introducida es: " + valor1 +" "+ operacion +" "+ valor2 + " = " + resultado );  
     }
     
 }
