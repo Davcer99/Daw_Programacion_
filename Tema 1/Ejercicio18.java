4a4dbe28ed65abe4a9a6811dae6b20c9501a39a9
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-06 10:21:31 +0200

    Ejercicio 18

--- Tema 1/Ejercicio18.java
+++ Tema 1/Ejercicio18.java
@@ -17,9 +17,23 @@ public class Ejercicio18 {
         segundos = entradaTeclado.nextByte();
 
         entradaTeclado.close();
-
-        
         segundos ++;
+
+        if (horas < 24 | minutos < 60 | segundos < 60){
+            if (horas >= 24){
+                horas = 00;
+            }
+            if (minutos >= 59) {
+                minutos = 00;
+                horas ++;
+            }
+            if (segundos >= 59){
+                segundos = 00;
+                minutos ++;
+            }
+        }else{
+            System.out.println("El formato de la hora introducido no es valido ");
+        }
         
         System.out.println(horas + ":" + minutos + ":" + segundos);
         
