cd044e92c1aeb46d5dd390fc259c82f06c0a4dac
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-08 10:42:52 +0200

    Subir ejercicio 20

---
+++ Tema 1/Ejercicio20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio20 {
+    
+    public static void main(String args[]){
+    
+        int precio;
+        
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        
+        System.out.println("Introduce el precio del producto ");
+        precio = entradaTeclado.nextByte();
+
+        entradaTeclado.close();
+
+        if ()
+
+    }
+        
+}
