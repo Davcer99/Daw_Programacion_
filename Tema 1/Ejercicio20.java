9a03cec317ccbd600c915dd8538d000296e00f3f
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-08 19:46:34 +0200

    Ejercicio 20 terminado

--- Tema 1/Ejercicio20.java
+++ Tema 1/Ejercicio20.java
@@ -4,7 +4,7 @@ public class Ejercicio20 {
     
     public static void main(String args[]){
     
-        int precio;
+        double precio, descuento5, descuento10;
         
         Scanner entradaTeclado;
         entradaTeclado = new Scanner(System.in);
@@ -14,8 +14,14 @@ public class Ejercicio20 {
 
         entradaTeclado.close();
 
-        if () {
-            
+        if ( precio >= 6 && precio < 60) {
+            descuento5 = precio * 5 / 100;
+            System.out.println("Se aplicara un descuento del 5% \nEl precio final del producto es = " + (precio - descuento5));
+        }else if (precio >=60) {
+            descuento10 = precio * 10 /100;
+            System.out.println("Se aplicara un descuento del 10% \nEl precio final del producto es = " + (precio - descuento10));
+        }else {
+            System.out.println("No se aplicara ningun descuento");
         }
 
     }
