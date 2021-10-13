365700eb24a516aafdf6d1345431eb823faebd33
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-13 09:43:46 +0200

    Control 01

---
+++ Tema 1/Ejercicio27.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio27 {
+    
+    public static void main(String args[]){
+    
+        int numero, = 1;
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        
+        System.out.println("Introduce el numero: ");
+        numero = entradaTeclado.nextInt();
+        entradaTeclado.close();
+
+        while (numero != 0){
+             
+            
+        }
+        
+        System.out.println("El resultado del factorial es: " + resultado);
+  
+
+    }
+        
+}
