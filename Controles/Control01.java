fb3d3d054a0edd3da03cd6f02579f34d855d9329
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-13 09:22:17 +0200

    Control 01

---
+++ Controles/Control01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Control01 {
+    
+    public static void main(String args[]){
+    
+        int lados;
+        double longitud;
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        
+        System.out.println("Dime el numero de lados del poligono: ");
+        lados = entradaTeclado.nextInt();
+        
+
+        if (lados == 3 || lados ==4){
+            System.out.println("Dime la longitud del lado del poligono: ");
+            longitud = entradaTeclado.nextDouble();
+            if (lados == 3){
+                System.out.println("El perimetro del triangulo es: " + longitud * 3);
+            }else{
+                System.out.println("El perimetro del cuadrado es: " + longitud * 4);
+            }
+        }
+        entradaTeclado.close();
+    }       
+}
