95336657f63d4edd41bbbc9e97b3b7e6ece143f8
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-18 09:07:58 +0200

    indexacion ejercicio 20 y subir ejercicio 31

--- Tema 1/Ejercicio30.java
+++ Tema 1/Ejercicio30.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio30 {
     
+    public static void main(String args[]){
+    
+        String palabra;
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+        
+        System.out.println("Introduce la palabra: ");
+        palabra = entradaTeclado.next();
+        entradaTeclado.close();
+
+        
+    }     
 }
