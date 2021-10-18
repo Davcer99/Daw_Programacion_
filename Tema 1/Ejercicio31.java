7137b7d55621a25e1d21643eb3d9f9362a63a4cf
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-18 09:21:18 +0200

    Ejercico 31

--- Tema 1/Ejercicio31.java
+++ Tema 1/Ejercicio31.java
@@ -1,3 +1,17 @@
 public class Ejercicio31 {
     
+    public static void main(String args[]){
+    
+        int[] dias;
+
+        dias = new int[7];
+        
+        dias[0] = 5;
+        dias[1] = 3;
+
+        for (int i = 0; i < 7; i++){
+            System.out.println(dias[i]);
+        }
+        
+    }     
 }
