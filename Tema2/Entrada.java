58dcd3a3c093f486dcc9073889bfb0eb30c3a4e5
Davcer99 <davidbalaguercervera@gmail.com>
2021-10-29 10:19:32 +0200

    Entrada

---
+++ Tema2/Entrada.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Entrada {
+
+    public static int getInteger (){
+        
+        int numero;
+        Scanner entradaTeclado;
+       
+        entradaTeclado = new Scanner(System.in);
+        while (true){
+            
+            System.out.println("Introduce un valor");
+            
+           if (entradaTeclado.hasNextInt()){
+                numero = entradaTeclado.nextInt();
+                break;
+            }else{
+                entradaTeclado.nextLine();
+            }
+        }
+        entradaTeclado.close();
+         
+        return numero;
+    }
+
+    public static void main (String args[]){
+      
+      getInteger();
+      
+    }
+
+}
