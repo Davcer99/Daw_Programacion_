dab88a15029ac194614dafe9e0e47c7829768166
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-02 17:56:35 +0100

    Ejercicio 35

--- Tema 1/Ejercico35.java
+++ Tema 1/Ejercico35.java
@@ -5,10 +5,11 @@ public class Ejercico35 {
         
         int nota, media = 0, total_notas= 1, numero_notas = 0;
         boolean fin = false, sobersaliente = false;
-        int[] notas ;
+        int[] notas = {0} ;
+        
         Scanner entradaTeclado;
         entradaTeclado = new Scanner (System.in);
-        notas = new int[20];
+        
         
        for (int i = 0; fin == false; i++) {
             System.out.println("Introduce tu nota, si quieres terminar introduce -1");
@@ -21,7 +22,7 @@ public class Ejercico35 {
                 fin = true;
             } 
         }
-        for (int l = 0; l < numero_notas; l++){
+        for (int l = 0; l < notas.length; l++){
 
             if (notas[l] == 10){
                 sobersaliente = true;
@@ -33,7 +34,7 @@ public class Ejercico35 {
         System.out.println("la nota media es: " + media);
 
         if (sobersaliente = true){
-            System.out.println("Has sacado un sobresaliente");
+            System.out.println("Has sacado un sobresaliente" + sobersaliente);
         }
     }  
 }
