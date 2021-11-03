3fb53014b850cab2b77e1f55b38be62ecf8195a2
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-03 08:55:21 +0100

    Cambios ejercoco 35 y 36

---
+++ Tema 1/Ejercicio35.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Ejercicio35 {
+
+    public static void main (String args[]){
+        
+        int nota, media = 0, total_notas= 1, numero_notas = 0;
+        boolean fin = false, sobersaliente = false;
+        int[] notas;
+        notas =new int [10];
+        
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner (System.in);
+        
+        
+       for (int i = 0; fin == false; i++) {
+            System.out.println("Introduce tu nota, si quieres terminar introduce -1");
+            nota = entradaTeclado.nextInt();
+
+            notas[i] = nota;
+            total_notas += nota;
+            numero_notas = i;
+            
+            if (notas[i] == -1){
+                fin = true;
+            } 
+        }
+        for (int l = 0; l < notas.length; l++){
+
+            if (notas[l] == 10){
+                sobersaliente = true;
+            }
+        }
+             
+        entradaTeclado.close();
+        media = total_notas / numero_notas;
+        System.out.println("la nota media es: " + media);
+
+        if (sobersaliente == true){
+            System.out.println("Has sacado un sobresaliente");
+        }
+    }  
+}
---
+++ Tema 1/Ejercicio36.java
@@ -0,0 +1,8 @@
+public class Ejercicio36 {
+
+    public static void main (String []args){
+
+        
+    }
+    
+}
