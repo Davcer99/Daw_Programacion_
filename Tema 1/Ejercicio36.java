897145b2caebef195630de9d092847c0df42285d
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-03 09:12:47 +0100

    Ejercicio 36

--- Tema 1/Ejercicio36.java
+++ Tema 1/Ejercicio36.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Ejercicio36 {
 
     public static void main (String []args){
 
-        
+        double[] numeros = new double [10];
+        double maximo = numeros [0];
+        Scanner lector = new Scanner (System.in);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Dame un numero");
+            numeros[i] = lector.nextDouble();
+            System.out.println("");
+        }
+
+        lector.close();
+
+        for(int i = 0; i < numeros.length; i++){
+            if (numeros[i] < maximo){
+                maximo = numeros[i];
+            }
+        }
     }
     
 }
