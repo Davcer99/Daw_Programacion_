c544869c960c2277e24e0d3b61d073c451d22064
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-03 10:40:54 +0100

    Ejercicio 40

---
+++ Tema 1/Ejercicio40.java
@@ -0,0 +1,16 @@
+public class Ejercicio40 {
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 1;
+        int i = 3, t;
+
+        System.out.print("1, 1");
+
+        while ( i <= 40){
+            t = n1 + n2;
+            System.out.print(", " + t);
+            n1 = n2;
+            n2 = t;
+            i += +1;
+        } 
+    }
+}
