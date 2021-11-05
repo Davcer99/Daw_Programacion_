8115a7e63e0775328313432375de469bd3641efb
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-05 10:44:51 +0100

    E08 comntrol

---
+++ Controles/Control02/E08.java
@@ -0,0 +1,24 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E08 {
+    public static void main (String []args){
+        
+
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+
+        System.out.print("Ingrese numero: ");
+        double numero = entradaTeclado.nextDouble();
+
+        entradaTeclado.close();
+       
+        long iPart = (long) numero;
+        double fPart = numero - iPart;
+    
+    
+
+
+
+
