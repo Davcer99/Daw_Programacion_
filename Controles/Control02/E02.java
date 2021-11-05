47a3d7f051016ab11e8faf8eef5ded488be7c4a6
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-05 09:19:55 +0100

    Ejercicio 41

---
+++ Controles/Control02/E02.java
@@ -0,0 +1,22 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E02 {
+    public static void main (String []args){
+
+        int area, perimetro;
+        
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+
+        System.out.println("Ingrese el radio: ");
+        int radio = entradaTeclado.nextInt();
+
+        entradaTeclado.close();
+       
+        perimetro = 2 * Mat
+         
+    }
+    
+}
