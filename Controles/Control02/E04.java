40956fb7bd011a0a13bf1db16ebe8f37434aa706
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-05 09:56:14 +0100

    E05 control 02

--- Controles/Control02/E04.java
+++ Controles/Control02/E04.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 public class E04 {
     public static void main (String []args){
-
-        double resultado;
         
         Scanner entradaTeclado;
         entradaTeclado = new Scanner(System.in);
 
-        System.out.print("Ingrese longitud: ");
-        double medida = entradaTeclado.nextDouble();
+        System.out.print("Ingrese numero: ");
+        String numero = entradaTeclado.next();
 
         entradaTeclado.close();
        
-        resultado = medida / 2.54f;
-
-        System.out.println(medida + "cm = " + resultado + "in" );  
+        for ( int i = 2; i != -1; i--){
+            System.out.print(numero.charAt(i));
+        } 
     }
 }
