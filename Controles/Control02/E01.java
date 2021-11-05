16968536ec814601996102c80e3bc81a704634dc
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-05 09:20:05 +0100

    dlkj

---
+++ Controles/Control02/E01.java
@@ -0,0 +1,18 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E01 {
+    public static void main (String []args){
+
+        Scanner entradaTeclado;
+        entradaTeclado = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre: ");
+        String nombre = entradaTeclado.nextLine();
+
+        entradaTeclado.close();
+       
+        System.out.println("Hola, " + nombre);  
+    }
+}
