c520edab9c49c1920ab5ee53de6eda50686560a1
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-05 10:05:22 +0100

    Correccion Ejercicios

--- Controles/Control02.java/E04.java
+++ Controles/Control02.java/E04.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class E04 {
     public static void main (String []args){
+
+        double resultado;
         
         Scanner entradaTeclado;
         entradaTeclado = new Scanner(System.in);
 
-        System.out.print("Ingrese numero: ");
-        String numero = entradaTeclado.next();
+        System.out.print("Ingrese cateto a: ");
+        double medida = entradaTeclado.nextDouble();
 
         entradaTeclado.close();
        
-        for ( int i = 2; i != -1; i--){
-            System.out.print(numero.charAt(i));
-        } 
+        resultado = medida / 2.54f;
+
+        System.out.println(medida + "cm = " + resultado + "in" );  
     }
 }
--- Controles/Control02.java/E05.java
+++ Controles/Control02.java/E05.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class E05 {
     public static void main (String []args){
-
-        double resultado;
         
         Scanner entradaTeclado;
         entradaTeclado = new Scanner(System.in);
 
-        System.out.print("Ingrese longitud: ");
-        double medida = entradaTeclado.nextDouble();
+        System.out.print("Ingrese numero: ");
+        String numero = entradaTeclado.next();
 
         entradaTeclado.close();
        
-        resultado = medida / 2.54f;
-
-        System.out.println(medida + "cm = " + resultado + "in" );  
-    }
+        for ( int i = 2; i != -1; i--){
+            System.out.print(numero.charAt(i));
+        } 
+    }  
+    
 }
