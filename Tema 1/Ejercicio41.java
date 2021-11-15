199e01358bc77aff818b1c585f05abbb7c29e161
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-15 09:17:33 +0100

    Ejercico 41 tema 1

--- Tema 1/Ejercicio41.java
+++ Tema 1/Ejercicio41.java
@@ -1,16 +1,17 @@
 public class Ejercicio41 {
     public static void main(String[] args) {
-        int n1 = 1, n2 = 1;
-        int i = 3, t;
+        double n1 = 1, n2 = 1;
+        double i = 3, t;
 
         System.out.print("1, 1");
 
         while ( i <= 40){
             t = n1 + n2;
             
-            System.out.print(", " + t);
+            
             n1 = n2;
             n2 = t;
+            System.out.print( " " + n2 / n1 );
             i += +1;
         } 
     }
