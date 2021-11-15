7ba49ff9e50c2c3ea106a39c6491c837f292aa24
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-15 10:11:37 +0100

    Ejercicio 43

--- Tema 1/Ejercicio43.java
+++ Tema 1/Ejercicio43.java
@@ -4,24 +4,21 @@ public class Ejercicio43 {
     
     
     public static void main(String[] args) {
+        
         Scanner lector = new Scanner(System.in);
-        int x,y;
+        int dividend,divisor;
 
-        System.out.print("Enter number 1:");
-        x = lector.nextInt();
+        System.out.print("Enter dividend 1: ");
+        dividend = lector.nextInt();
 
-        System.out.print("Enter number 2:");
-        y = lector.nextInt();
+        System.out.print("Enter divisor 2: ");
+        divisor = lector.nextInt();
         lector.close(); 
 
-        int mult = x;
-        int i = 1;
-
-        while (i < y){
-            mult += y;
-            i ++;
+        while (dividend >= divisor){
+            dividend -= divisor;
         }
 
-        System.out.println("result = " + mult);
+        System.out.println("rest = " + dividend);
     } 
 }
