0f9de7e9571353e6ab598fd37b87bc5f8954f62f
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-15 08:44:31 +0100

    Mayor de tres ejercicio tema 2

--- Tema2/MayorDeTres.java
+++ Tema2/MayorDeTres.java
@@ -1,3 +1,15 @@
 public class MayorDeTres {
-    
+    public static void mayor (int n1, int n2, int n3){
+        if (n1 > n2 & n1 > n3){
+            System.out.println(n1);
+        }else if (n2 > n1 & n2 > n3){
+            System.out.println(n2);
+        }else {
+            System.out.println(n3);
+        }
+    }
+
+    public static void main(String[] args) {
+        mayor(2, 8, 4);
+    }
 }
