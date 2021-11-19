49c561f74f17d97f31e584dcdd10dd979386e46e
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-19 10:31:21 +0100

    Ejercico46

---
+++ Tema 1/Ejercico46.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercico46 {
+    public static void main(String[] args) {
+        Scanner entradaTeclado = new Scanner(System.in);
+        int numero;
+        int max = 100, min = 1;
+    // Producir nuevo int aleatorio entre max y min    
+    int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+
+        System.out.println("Bienvenido al juego de adivinar el numero.");
+
+        while (true){
+            System.out.println("Introducen un numero");
+            numero = entradaTeclado.nextInt();
+
+            if(numero == -1){
+                break;
+            }else if (numero < aleatorio){
+                System.out.println("No has adivinado el numero :(, Pista: el numero es mayor al que has introducido");
+            }else if (numero > aleatorio){
+                System.out.println("No has adivinado el numero :(, Pista: el numero es menor al que has introducido");
+            }else if (numero == aleatorio){
+                System.out.println("Enhorabuena, Has ganado.");
+                break;
+            }
+        }
+        entradaTeclado.close();
+    }
+}
