8b5da93ba26cce83093edfbd8b237f9f1bea193f
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-22 09:05:23 +0100

    Ejercicio47

--- Tema 1/Ejercicio47.java
+++ Tema 1/Ejercicio47.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class Ejercicio47 {
     public static void main(String[] args){
+        int tamaño = 0;
         String[] cadenas = new String[5];
         System.out.println("Introduce 5 cadenas de texto:");
         Scanner miScanner;
         miScanner = new Scanner(System.in);
+        miScanner.close();
         
         for (int i = 0; i < cadenas.length; i++){
             cadenas[i] = miScanner.next();
         }
 
+        for (int i = 0; i < cadenas.length; i++){
+           if ( tamaño > cadenas[i].length()) {
+                tamaño = cadenas[i].length();
+            }
+        }
+
+        System.out.println();
     }
 }
