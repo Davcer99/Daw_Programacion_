248a41418f9e31fe65dcdbfae0ddbf3c2e3c5e87
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-24 09:11:13 +0100

    Ejercico 1 tema 3

--- Tema3/Eliminar.java
+++ Tema3/Eliminar.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
-
 class Eliminar {
     
     public static void main(String[] args) {
         int [] numeros = {1,2,3,4,5,6,7,8,9,10};
         int valor;
-        Scanner es = new Scanner(System.in);
 
         while(true){
-            System.out.println("Introduce el valor del array que quieres eliminar valor del 0 al 9");
-            valor = es.nextInt();
-            es.close();
 
+            valor = Utilidades.leerEntero("introduce el valor que deseas eliminar");
+            
             if (valor > numeros.length){
                 System.out.println("El valor introducido es mayor al tamaño del array");
+            }else{
+                break;
             }
         }   
-        if (){
+        if (valor < numeros.length){
             numeros[valor] = 0;
             
             for (int i = 0; i < numeros.length; i++) {
