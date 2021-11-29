5a0a166b592520b81a2cec03ec9358c474a63402
Davcer99 <davidbalaguercervera@gmail.com>
2021-11-29 09:34:49 +0100

    Ejercicio duplicados y comunes tema 3

--- Tema3/Comunes.java
+++ Tema3/Comunes.java
@@ -2,21 +2,19 @@ public class Comunes {
     public static void main(String[] args) {
         int [] n1= {1,2,3,4};
         int [] n2= {5,6,3,2};
-        int [] numeros_duplicados = {0,0,0};
-        int cont = 0, j;
+        int j;
+
+        System.out.print("Los numeros duplicados son los siguientes: ");
         
         for (int i = 0; i < n1.length; i++) {
 
             for (j = 0; j < n1.length; j++) {
                 
                 if (n1[i] == n2[j]){
-                    numeros_duplicados[cont] = n2[j];
-                    cont++;
+                    System.out.print(n2[j] + " ");
                 } 
             }
             j= 0;
         }
-        //imprimimos el array con los numeros duplicados
-        Utilidades.mostrarArray(numeros_duplicados);
     }
 }
--- Tema3/Duplicados.java
+++ Tema3/Duplicados.java
@@ -2,22 +2,17 @@ public class Duplicados {
     public static void main(String[] args) {
 
         int[] numeros = {1,2,3,4,5,2,3};
-        int [] numeros_duplicados =new int [4];
-        int cont = 0;
+
+        System.out.print("Los numeros duplicados son los siguientes: ");
+        
         for (int i = 0; i < numeros.length; i++) {
 
             for (int j = i + 1; j < numeros.length; j++) {
                 
                 if (numeros[i] == numeros[j]){
-                    numeros_duplicados[cont] = numeros[j];
-                    cont++;
+                    System.out.println(numeros[j]);
                 } 
             }
         }
-        System.out.print("Los numeros duplicados son los siguientes: ");
-        for (int i = 0; i < numeros_duplicados.length; i++) {
-            System.out.print(numeros_duplicados [i] + ", "); 
-        }
-        
     }  
 }
