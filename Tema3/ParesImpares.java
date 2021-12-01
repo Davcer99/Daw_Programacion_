2e04400534fb8e8a9010b8aee7671182c3c25c85
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 09:27:06 +0100

    Ejercicio pares Impares Tema 3

--- Tema3/ParesImpares.java
+++ Tema3/ParesImpares.java
@@ -1,11 +1,23 @@
 public class ParesImpares {
     public static void main(String[] args) {
         int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+        int[] pares ={0,0,0,0,0};
+        int[] impares ={0,0,0,0,0};
+        int cn1=0,cn2=0;
 
         for (int i = 0; i < numeros.length; i++) {
             if ((numeros[i] % 2) == 0){
-                
+                pares[cn1] = numeros[i];
+                cn1++;
+            }else{
+                impares[cn2] = numeros[i];
+                cn2++;
             }
         }
+        System.out.print("Los numeros pares son: ");
+        Utilidades.mostrarArray(pares);
+        System.out.print("\nLos numeros impares son: ");
+        Utilidades.mostrarArray(impares);
+        
     }
 }
