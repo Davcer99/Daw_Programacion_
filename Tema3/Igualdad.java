5a02d396b28af00622e2749904ea417d7b56d0d0
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 08:33:03 +0100

    Ejercicio igualdad tema 3

--- Tema3/Igualdad.java
+++ Tema3/Igualdad.java
@@ -2,14 +2,19 @@ public class Igualdad {
     public static void main(String[] args) {
         int[] numeros = {1,2,3,4};
         int[] copia = numeros.clone();
-        
+        boolean iguales = false;
 
         for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] == copia[i] ){
-
+                iguales = true;
             }else{
+                iguales = false;
                 System.out.println("No son iguales");
+                break;
             }
+        }
+        if (iguales == true){
+            System.out.println("Los dos vectores son iguales");
         }
     }
 }
