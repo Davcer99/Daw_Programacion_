9cdc0ce1b0203d204e7141a4e9ab6470a5517adb
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 10:30:22 +0100

    Rellenar Matriz Tema 3

---
+++ Tema3/RellenaMatriz.java
@@ -0,0 +1,15 @@
+public class RellenaMatriz {
+    public static void main(String[] args) {
+        int[][] matriz;
+        matriz = new int [10][10];
+
+        Utilidades.rellenaMatriz(matriz, 0, 1);
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] +  " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
