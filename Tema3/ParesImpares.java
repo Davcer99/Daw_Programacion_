81bb7688b5cefb7d78b837a18c29a05c69003bd2
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 08:54:16 +0100

    Ejercicio pares impares tema 3

---
+++ Tema3/ParesImpares.java
@@ -0,0 +1,11 @@
+public class ParesImpares {
+    public static void main(String[] args) {
+        int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i = 0; i < numeros.length; i++) {
+            if ((numeros[i] % 2) == 0){
+                
+            }
+        }
+    }
+}
