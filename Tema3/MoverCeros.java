e3e20a54084d9645831027e764f831e2bc060b6e
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 09:56:16 +0100

    Ejercicio Mover ceros Tema 3

--- Tema3/MoverCeros.java
+++ Tema3/MoverCeros.java
@@ -1,3 +1,16 @@
 public class MoverCeros {
-    
+    public static void main(String[] args) {
+       int[] numeros = {0,1,0,2,0,3,0,4};
+
+       //Recorremos el array para buscar los ceros 
+       for (int i = 0; i < numeros.length; i++) {
+
+           if (numeros[i] == 0){
+               //Con la utilidad elemento hecha anteriormente borramos el cero y lo ponemos al final del array
+                Utilidades.eliminarElemento(numeros, i);
+            }
+        }
+        //Imprimimos el array resultante
+        Utilidades.mostrarArray(numeros);
+    } 
 }
