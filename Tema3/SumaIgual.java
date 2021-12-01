2b4e70485bcb047958d7a7bf0d083c515c4d3c94
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-01 09:06:15 +0100

    Ejercicio suma igual Tema 3

--- Tema3/SumaIgual.java
+++ Tema3/SumaIgual.java
@@ -1,19 +1,18 @@
 public class SumaIgual {
     public static void main(String[] args) {
         int [] n1= {1,2,3,4};
-        int [] n2= {5,6,3,2};
-        int j, valor = 3;
+        int j, valor = 5;
 
 
         for (int i = 0; i < n1.length; i++) {
 
-            for (j = 0; j < n1.length; j++) {
+            for (j = i + 1; j < n1.length; j++) {
                 
-                if (n1[i] + n2[j] == valor){
-                    System.out.println(n1[i] + " + " + n2[j]);
+                if ((n1[i] + n1[j]) == valor){
+                    System.out.println(n1[i] + " + " + n1[j]);
                 } 
             }
-            j= 0;
+            
         } 
     }
 }
