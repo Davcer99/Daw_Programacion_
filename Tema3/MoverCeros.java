b9b96a41a0f3483e407e870fed7152831e54afdd
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-03 08:30:04 +0100

    Ejercicio mover ceros tema 3

--- Tema3/MoverCeros.java
+++ Tema3/MoverCeros.java
@@ -3,14 +3,21 @@ public class MoverCeros {
        int[] numeros = {0,0,0,2,0,3,0,4};
 
        //Recorremos el array para buscar los ceros 
-       for (int i = 0; i < numeros.length; i++) {
-
+       for (int i = numeros.length -1; i >= 0; i--) {
            if (numeros[i] == 0){
-               //Con la utilidad elemento hecha anteriormente borramos el cero y lo ponemos al final del array
-                Utilidades.eliminarElemento(numeros, i);
-            }
+            moverCeroDerecha (numeros, i);
+           }
+            
         }
         //Imprimimos el array resultante
         Utilidades.mostrarArray(numeros);
+    }
+
+    public static void moverCeroDerecha(int[] numeros, int pos) {
+        //Recorre el array desplazando los valores hacia la izquierda 
+        for (int i = pos; i < numeros.length; i++) {
+            numeros[i] = numeros [i + 1];
+            numeros [i + 1] = 0;
+        }
     } 
 }
