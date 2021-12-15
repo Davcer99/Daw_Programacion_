44469d071d065677c1dc9c0def98a7cd1248e1ad
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-15 08:25:48 +0100

    Ejercicio longitud tema3

--- Tema3/Longitud.java
+++ Tema3/Longitud.java
@@ -1,9 +1,14 @@
 public class Longitud {
+    public static String[] dividirPalabras (String cadenaDePalabras){
+        String[] palabras;
+        palabras = Utilidades.dividirEnPalabras(cadenaDePalabras);
+        return palabras;
+    }
     public static void main(String[] args) {
         String[] palabras;
         String cadenaDePalabras = "Hola que tal estas";
 
-        palabras = Utilidades.dividirEnPalabras(cadenaDePalabras);
+        palabras = dividirPalabras(cadenaDePalabras);
 
         for (int i = 0; i < palabras.length; i++) {
             System.out.println(palabras[i] + " Tiene " + palabras[i].length() + " caracteres");
