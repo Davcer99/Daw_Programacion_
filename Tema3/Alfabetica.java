0822f06e170e7f0154f77356786ef7a55f2c22d5
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-20 09:12:14 +0100

    modificar error

--- Tema3/Alfabetica.java
+++ Tema3/Alfabetica.java
@@ -3,6 +3,7 @@ public class Alfabetica {
     public static boolean esAlfabetica (String palabra) {
         
         for (int i = 1; i < palabra.length(); i++) {
+
             if(palabra.charAt(i-1) < palabra.charAt(i)){
 
             }else{
@@ -16,8 +17,6 @@ public class Alfabetica {
             System.out.println("La palabra es Alfabetica");
         }else{
             System.out.println("La palabra no es alfabetica");
-        }
-
-        
+        } 
     }
 }
