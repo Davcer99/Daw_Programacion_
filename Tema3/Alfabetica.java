9907289bcd61d1232791b1ab5f0cb1856079bbb7
Davcer99 <davidbalaguercervera@gmail.com>
2021-12-20 08:57:48 +0100

    Ejercicio Alfabetico Tema 3

--- Tema3/Alfabetica.java
+++ Tema3/Alfabetica.java
@@ -1,5 +1,23 @@
 public class Alfabetica {
+
+    public static boolean esAlfabetica (String palabra) {
+        
+        for (int i = 1; i < palabra.length(); i++) {
+            if(palabra.charAt(i-1) < palabra.charAt(i)){
+
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
+        if (esAlfabetica("chino")){
+            System.out.println("La palabra es Alfabetica");
+        }else{
+            System.out.println("La palabra no es alfabetica");
+        }
+
         
     }
 }
