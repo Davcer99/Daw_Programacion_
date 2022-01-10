6285a15159aaaca797b873f6140fba12cc580103
Davcer99 <davidbalaguercervera@gmail.com>
2022-01-10 09:44:39 +0100

    Ejercicio encriptar desencriptar

---
+++ Tema3/Desencriptar.java
@@ -0,0 +1,16 @@
+public class Desencriptar {
+    public static void main(String[] args) {
+        String cadena = Utilidades.leerCadena("Introduce el texto que deseas encriptar");
+        int n = Utilidades.leerEntero("Introduce la n");
+        String resultado = "";
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            char letra;
+            letra =cadena.charAt(i);
+            letra -= n; 
+            resultado += letra;
+        }
+
+        System.out.println(resultado);
+    }
+}
