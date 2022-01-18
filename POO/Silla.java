9193e3b37b4b7fe470584653f2009f21d27d90a5
Davcer99 <davidbalaguercervera@gmail.com>
2022-01-18 19:39:35 +0100

    Ejercicio silla

--- POO/Silla.java
+++ POO/Silla.java
@@ -1,3 +1,26 @@
 public class Silla {
-    /* Crear una clase silla que no tendra main atributos de las sillas el color nimero de patas si tiene respaldo o no nivel de comodidad de 0 al 10 material de construccion ademas la clase silla tiene un punto que las ubica en una habitación con x e y hay que definir todos los atributos como privados pero desde la clase test se deben poder modificar e acceder a su valor crear en la clase de prueba 100 sillas con distintas posiciones , materiales etc*/
+    /* Crear una clase silla que no tendra main, atributos de las sillas el color numero de patas si tiene respaldo o no nivel de comodidad de 0 al 10 material de construccion ademas la clase silla tiene un punto que las ubica en una habitación con x e y hay que definir todos los atributos como privados pero desde la clase test se deben poder modificar e acceder a su valor crear en la clase de prueba 100 sillas con distintas posiciones , materiales etc*/
+
+    // atributos 
+    private String color;
+    private int nPatas;
+    private boolean respaldo;
+    private int nivelComodidad;
+    private String material;
+    private int [] posicion = {0,0};
+
+
+    public Silla (String color, int nPatas, boolean respaldo, int nivelComodidad, String material, int posX, int posY){
+        this.color = color;
+        this.nPatas = nPatas;
+        this.respaldo = respaldo;
+        this.nivelComodidad = nivelComodidad;
+        this.material = material;
+        this.posicion [1] = posX;
+        this.posicion [2] = posY;
+    }
+
+    public void imprimirValores (){
+        System.out.println("Color de la silla " + silla.color + "\nNumero de patas de la silla " + silla.nPatas + "\nTiene respaldo " + silla.respaldo + "\nNivel de comodidad de la silla " + silla.nivelComodidad + "\nMaterial de la silla " + silla.material + "Posición de la silla " + silla.posicion [1] + " , " + silla.posicion [2]);
+    }
 }
