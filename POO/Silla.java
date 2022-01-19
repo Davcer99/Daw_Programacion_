32d5d7db5a344446c88588027e4446c5c0422d98
Davcer99 <davidbalaguercervera@gmail.com>
2022-01-19 10:37:31 +0100

    Ejercicio silla terminado

--- POO/Silla.java
+++ POO/Silla.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.util.Random;
 public class Silla {
     /* Crear una clase silla que no tendra main, atributos de las sillas el color numero de patas si tiene respaldo o no nivel de comodidad de 0 al 10 material de construccion ademas la clase silla tiene un punto que las ubica en una habitación con x e y hay que definir todos los atributos como privados pero desde la clase test se deben poder modificar e acceder a su valor crear en la clase de prueba 100 sillas con distintas posiciones , materiales etc*/
 
@@ -17,6 +18,28 @@ public class Silla {
         this.respaldo = respaldo;
         this.nivelComodidad = i;
         this.material = material;
+        this.ubicacion = ubicacion;
+    }
+
+    public Silla (){
+        
+        Random ran = new Random();
+        String [] material = {"madera", "alumninio", "plastico", "inoxidable"};
+        String [] color = {"Rojo", "Amarillo", "Verde", "Morado", "Marron", "Azul "};
+        boolean respaldo = false;
+        Point ubicacion = new Point (ran.nextInt(100-0+1)+0, ran.nextInt(100-0+1)+0);
+
+        if (ran.nextInt(1-0+1)+0 == 1){
+            respaldo = true;
+        }else {
+            respaldo = false;
+        }
+
+        this.color = color[ran.nextInt(5-0+1)+0];
+        this.nPatas = ran.nextInt(6-3+1)+3;
+        this.respaldo = respaldo;
+        this.nivelComodidad = ran.nextInt(10-0+1)+0;
+        this.material = material[ran.nextInt(3-0+1)+0];
         this.ubicacion = ubicacion;
     }
 
--- POO/SillaTest.java
+++ POO/SillaTest.java
@@ -1,28 +1,11 @@
-import java.util.Random;
-import java.awt.Point;
 
 public class SillaTest {
     public static void main(String[] args) {
 
-        Random ran = new Random();
-        String [] material = {"madera", "alumninio", "plastico", "inoxidable"};
-        String [] color = {"Rojo", "Amarillo", "Verde", "Morado", "Marron", "Azul "};
-        
         for (int i = 0; i < 100; i++) {
-
-            System.out.println("\nSilla numero " + i + "\n");
-            boolean respaldo = false;
-            Point ubicacion = new Point (ran.nextInt(100-0+1)+0, ran.nextInt(100-0+1)+0);
-
-            if (ran.nextInt(1-0+1)+0 == 1){
-                respaldo = true;
-            }else {
-                respaldo = false;
-            }
-
-            Silla iSilla = new Silla(color[ran.nextInt(5-0+1)+0], 4, respaldo, ran.nextInt(10-0+1)+0, material[ran.nextInt(3-0+1)+0], ubicacion);
-        
-            iSilla.imprimirValores(); 
+            Silla silla = new Silla();
+            System.out.println( "Silla " + i + "\n");
+            silla.imprimirValores();
         }  
     }
 }
