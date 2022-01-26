86bd5415ff083fc0fe5daef6aa55ea84ebdf368b
Davcer99 <davidbalaguercervera@gmail.com>
2022-01-26 09:40:35 +0100

    Ejercicio ticket machine

--- POO/TicketMacine2Test.java
+++ POO/TicketMacine2Test.java
@@ -1,6 +1,6 @@
 public class TicketMacine2Test {
     public static void main(String[] args) {
-        TicketMachine2 m1, m2;
+        TicketMachine2 m1;
 
         m1 = new TicketMachine2(30);
         m1.insertMoney(50);
