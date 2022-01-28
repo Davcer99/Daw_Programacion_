44fd958ac3dab39bc96e895031559ad7983ad068
Davcer99 <davidbalaguercervera@gmail.com>
2022-01-28 10:43:47 +0100

    Ejercicio clock

---
+++ POO/ClockTest.java
@@ -0,0 +1,11 @@
+import java.time.Clock;
+
+public class ClockTest {
+
+    public static void main(String[] args) {
+        ClockDisplay c1 ;
+        c1 = new ClockDisplay(11,60, 55);
+        c1.
+    }
+    
+}
