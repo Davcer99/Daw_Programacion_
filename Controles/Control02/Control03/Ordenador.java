4e028934e3436c4aec5465bd889f6c13f65adcae
Davcer99 <davidbalaguercervera@gmail.com>
2022-02-04 10:44:08 +0100

    Ejercicio Ordenador

---
+++ Controles/Control02/Control03/Ordenador.java
@@ -0,0 +1,58 @@
+package Control02.Control03;
+
+public class Ordenador {
+
+    private String formato;
+    private String color;
+    private Teclado teclado;
+    private Ordenador pcConectado = null;
+
+    
+
+    public Ordenador(String formato, String color, Teclado teclado) {
+        this.formato = formato;
+        this.color = color;
+        this.teclado = teclado;
+    }
+
+    public Ordenador(){
+        this.formato = "Sobremesa";
+        this.color = "Blanco";
+        this.teclado = new Teclado();
+    }
+
+    // getters y setters
+    public String getFormato() {
+        return formato;
+    }
+    public String getColor() {
+        return color;
+    }
+    public Teclado getTeclado() {
+        return teclado;
+    }
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    //imprimir
+    public void imprimir (){
+        System.err.println("Formato: " + this.formato);
+        System.out.println("Color: " + this.color);
+        this.teclado.print();
+        if (pcConectado == null){
+            System.out.println("No hay ordenadores conectados");
+        }else {
+            System.out.println("Ordenador conectado: " + this.pcConectado);
+        }
+    }
+
+    //coexion con otro pc
+    public void conectar(Ordenador pcConectado) {
+        if (pcConectado == null){
+            this.pcConectado = pcConectado; 
+        }else{
+            System.out.println("Ya hay un pc conectado");
+        }  
+    }
+}
