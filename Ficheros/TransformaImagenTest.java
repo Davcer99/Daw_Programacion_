83111494d169a5671751ad91f3ae927bb909917d
Davcer99 <davidbalaguercervera@gmail.com>
2022-04-08 08:57:47 +0200

    Ejercicio Transformar imagenes

---
+++ Ficheros/TransformaImagenTest.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+public class TransformaImagenTest {
+    public static void main(String[] args) {
+        File image = new File("./penyagolosa.bmp");
+        TransformaImagen ti = new TransformaImagen(image);
+        try {
+            ti.transformaNegativo();
+            ti.transformaNegroBlanco();
+            ti.transformaOscuro();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
