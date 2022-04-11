3f1fbffb83519ecd2a8dcedb3fb7c2466b2ff7d6
Davcer99 <davidbalaguercervera@gmail.com>
2022-04-11 08:16:28 +0200

    identar codigo

--- Ficheros/LeerPrimos.java
+++ Ficheros/LeerPrimos.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 
 
 public class LeerPrimos {
-     public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        FileReader f_in = new FileReader("./archivos/primos.txt");
-        BufferedReader bf_1 = new BufferedReader(f_in);
-        
-        String c = bf_1.readLine();
-        
-        while (c != null) {
-          System.out.println(c); 
-          c = bf_1.readLine(); 
-        }
-        f_in.close();
+    FileReader f_in = new FileReader("./archivos/primos.txt");
+    BufferedReader bf_1 = new BufferedReader(f_in);
+    
+    String c = bf_1.readLine();
+    
+    while (c != null) {
+      System.out.println(c); 
+      c = bf_1.readLine(); 
     }
+    f_in.close();
+  }
     
 }
 
