1cba304c58542d560eee06165e85b718a587d08f
Davcer99 <davidbalaguercervera@gmail.com>
2022-06-03 09:46:18 +0200

    Ejercicio insertar bases de datos

--- BasesDeDatos/ConsultaBases.java
+++ BasesDeDatos/ConsultaBases.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 /**
  * TestDB
@@ -6,7 +7,7 @@ import java.sql.*;
 public class ConsultaBases {
 
     public static void main(String[] args) {
-        String consulta = "SELECT count(*) as total FROM Cliente";
+        String consulta = "INSERT INTO Cliente (nombre,apellidos,email,nif) VALUES (?,?,?,?)";
         String connectionUrl = "jdbc:mysql://10.0.2.4/clientes";
 
         try {
@@ -15,15 +16,27 @@ public class ConsultaBases {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insertar nombre");
+        String nombre = sc.nextLine();
+        System.out.println("Insertar apellidos");
+        String apellidos = sc.nextLine();
+        System.out.println("Insertar email");
+        String email = sc.nextLine();
+        System.out.println("Insertar nif");
+        String nif = sc.nextLine();
+        sc.close();
+    
+        try  {
+            Connection conn = DriverManager.getConnection(connectionUrl, "phpmyadmin", "phpmyadmin");
+            PreparedStatement ps = conn.prepareStatement(consulta);
+            ps.setString(1, nombre);
+            ps.setString(2, apellidos);
+            ps.setString(3, email);
+            ps.setString(4, nif);
+            ps.executeUpdate();
+            System.out.println("Insert realizado con exito");
 
-        try (Connection conn = DriverManager.getConnection(connectionUrl, "phpmyadmin", "phpmyadmin");
-                PreparedStatement ps = conn.prepareStatement(consulta);
-                ResultSet rs = ps.executeQuery()) {
-
-            while (rs.next()) {
-                int total = rs.getInt("total");
-                System.out.println("Total: " + total);
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
